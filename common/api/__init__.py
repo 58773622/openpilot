84670685775b6b73c854e1b06d36078abdc10499
58773622 <59197362+58773622@users.noreply.github.com>
2026-02-01 08:53:39 +0800

    更改服务器地址

--- common/api/__init__.py
+++ common/api/__init__.py
@@ -5,7 +5,8 @@ from datetime import datetime, timedelta
 from openpilot.system.hardware.hw import Paths
 from openpilot.system.version import get_version
 
-API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
+API_HOST = os.getenv('API_HOST', 'http://skyunlock.cn:5009')
+KONIK_API_HOST = os.getenv('API_HOST', 'http://skyunlock.cn:5009')
 
 class Api:
   def __init__(self, dongle_id):
